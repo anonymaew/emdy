f767b29e503a24b561c4491e21db480b3d17fe9c
Napat Srichan <napatsrichan2001@gmail.com>
2023-03-23 02:57:36 -0700

    fix relative path not working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,24 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import { astroImageTools } from 'astro-imagetools';
-
 const defaultLayoutPlugin = () => {
-    return (tree, file) => {
-        file.data.astro.frontmatter.layout = '/src/layouts/main.astro';
-    };
+  return (tree, file) => {
+    file.data.astro.frontmatter.layout = '/src/layouts/main.astro';
+  };
 };
+
 
 // https://astro.build/config
 export default defineConfig({
-    publicDir: './src/pages',
-    integrations: [
-        astroImageTools,
-        tailwind(),
-    ],
-	markdown: {
-		remarkPlugins: [
-            defaultLayoutPlugin,
-            'remark-code-titles',
-            'remark-math',
-        ],
-		rehypePlugins: [
-			'rehype-slug',
-			['rehype-autolink-headings', { behavior: 'prepend' }],
-			['rehype-toc', { headings: 'h2,h3,h4' }],
-            'rehype-mathjax',
-		],
-	},
+  publicDir: './src/pages',
+  smartyPants: false,
+  integrations: [astroImageTools, tailwind()],
+  markdown: {
+    remarkPlugins: [defaultLayoutPlugin, 'remark-code-titles', 'remark-math'],
+    rehypePlugins: ['rehype-slug', ['rehype-autolink-headings', {
+      behavior: 'prepend'
+    }], ['rehype-toc', {
+      headings: 'h2,h3,h4'
+    }], 'rehype-mathjax']
+  }
 });
